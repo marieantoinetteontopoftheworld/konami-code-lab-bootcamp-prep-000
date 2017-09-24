6a93dcd6c2dcf1e528287e096935c6abbaf3cfd6
marieantoinetteontopoftheworld <jack.heseltine@gmail.com>
2017-09-24 19:50:52 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,12 +1,13 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+
+var index = 0; // tracker
 
 function init() {
-  let index = 0;
-
+  // Write your JavaScript code inside the init() function
   document.body.addEventListener('keydown', function(e) {
-    const key = parseInt(e.which || e.detail);
+    const key = parseInt(e.detail || e.which);
 
-    if (code[index] === key) {
+    if (key === code[index]) {
       index++;
 
       if (index === code.length) {
@@ -18,4 +19,5 @@ function init() {
       index = 0;
     }
   });
+
 }
