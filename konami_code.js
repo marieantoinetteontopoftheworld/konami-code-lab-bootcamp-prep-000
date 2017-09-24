995cadce7cd470a2739579ca8377b7a41d9fb728
marieantoinetteontopoftheworld <jack.heseltine@gmail.com>
2017-09-24 19:45:58 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -6,11 +6,9 @@ function init() {
   // Write your JavaScript code inside the init() function
   document.body.addEventListener('keydown', function(e) {
     const key = parseInt(e.detail || e.which);
-    console.log(e.detail);
 
     if (key === code[index]) {
       index++;
-      console.log(index);
 
       if (index === code.length) {
         alert('Konami!');
